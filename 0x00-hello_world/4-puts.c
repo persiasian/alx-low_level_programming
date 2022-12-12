be4ce56d16ceb53ed87382c8dfe18c5e4f62c665
root <ianokello@outlook.com>
2022-12-12 05:03:49 -0800

    prints programming...

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
+/** main entry point*/
+
 int main(void)
 {
-	puts("Programming is like bulding a multiligual puzzle");
+	puts( "Programming is like bulding a multiligual puzzle" );
 	
 	return(0);
-
 }
 
